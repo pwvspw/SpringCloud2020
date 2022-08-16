4af65d67ee711fc8e33ebb93425335adf1a7ee45
pwvspw <pengwei@dztec.net>
2022-08-16 11:28:14 +0800

    spring cloud stream消息驱动之消费者1

---
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/atguigu/springcloud/service/ReceiveMessageListener.java
@@ -0,0 +1,25 @@
+package com.atguigu.springcloud.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@EnableBinding(Sink.class)
+public class ReceiveMessageListener {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @StreamListener(Sink.INPUT)
+    public void input(Message<String> message) {
+        String payload = message.getPayload();
+        log.info("消费者1号，接收到的消息是：" + payload + "\t服务端口号：" + serverPort);
+    }
+
+}
