467407c5c41a25e680dd35c093c3122edb9724aa
pwvspw <pengwei@dztec.net>
2022-08-16 22:16:53 +0800

    rename main application

--- cloudalibaba-provider-payment9001/src/main/java/com/atguigu/springcloud/PaymentNacosMain9001.java
+++ cloudalibaba-provider-payment9001/src/main/java/com/atguigu/springcloud/PaymentNacosMain9001.java
@@ -4,12 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
 @EnableDiscoveryClient
-public class PaymentMain9002 {
+@SpringBootApplication
+public class PaymentNacosMain9001 {
 
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain9002.class, args);
+        SpringApplication.run(PaymentNacosMain9001.class, args);
     }
 
 }
--- cloudalibaba-provider-payment9002/src/main/java/com/atguigu/springcloud/PaymentNacosMain9002.java
+++ cloudalibaba-provider-payment9002/src/main/java/com/atguigu/springcloud/PaymentNacosMain9002.java
@@ -4,12 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
 @SpringBootApplication
-public class PaymentMain9001 {
+@EnableDiscoveryClient
+public class PaymentNacosMain9002 {
 
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain9001.class, args);
+        SpringApplication.run(PaymentNacosMain9002.class, args);
     }
 
 }
