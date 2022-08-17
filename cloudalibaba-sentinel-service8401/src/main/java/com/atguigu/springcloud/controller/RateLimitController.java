16f14008982f88d4b597fe684710318a85500bb2
pwvspw <pengwei@dztec.net>
2022-08-17 21:22:05 +0800

    spring cloud alibaba sentinel按URL地址限流

--- cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/controller/RateLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/controller/RateLimitController.java
@@ -13,11 +13,17 @@ public class RateLimitController {
     @GetMapping("/byResource")
     @SentinelResource(value = "byResource", blockHandler = "handleException")
     public CommonResult<Payment> byResource() {
-        return new CommonResult<>(200, "按资源名称限流OK", new Payment(2022L, "123456"));
+        return new CommonResult<>(200, "按资源名称限流OK", new Payment(2022L, "serial001"));
     }
 
     public CommonResult<Payment> handleException(BlockException exception) {
         return new CommonResult<>(444 ,exception.getClass().getCanonicalName() + "  服务不可用");
+    }
+
+    @GetMapping("/rateLimit/byUrl")
+    @SentinelResource(value = "byUrl")
+    public CommonResult<Payment> byUrl() {
+        return new CommonResult<>(200, " 按 url 限流测试 OK", new Payment(2022L, "serial002"));
     }
 
 }
